7f4d1f41cc615e7f846bce8a50bfdd38faccf585
M Akbar Maulana <makbarm.id@gmail.com>
2023-01-16 18:15:58 +0700

    ADD : Add book feature

---
+++ src/handler.js
@@ -0,0 +1,67 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+
+  if (!name) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. Mohon isi nama buku',
+      })
+      .code(400);
+  }
+
+  if (readPage > pageCount) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+      })
+      .code(400);
+  }
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished: pageCount === readPage,
+    reading,
+    insertedAt,
+    updatedAt: insertedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.find((book) => book.id === id);
+
+  if (isSuccess) {
+    return h
+      .response({
+        status: 'success',
+        message: 'Buku berhasil ditambahkan',
+        data: { bookId: id },
+      })
+      .code(201);
+  }
+
+  return h
+    .response({
+      status: 'error',
+      message: 'Buku gagal ditambahkan',
+    })
+    .code(500);
+};
+
+module.exports = { addBookHandler };
