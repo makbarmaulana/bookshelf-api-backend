bf4fec4739d1bd0fbc879465e41736ea98e3dfd2
M Akbar Maulana <makbarm.id@gmail.com>
2023-01-16 19:44:14 +0700

    ADD : Get book by id features

--- src/handler.js
+++ src/handler.js
@@ -69,4 +69,26 @@ const getAllBooksHandler = () => ({
   data: { books },
 });
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const book = books.filter((b) => b.id === id)[0];
+
+  if (book !== undefined) {
+    return h
+      .response({
+        status: 'success',
+        data: { book },
+      })
+      .code(200);
+  }
+
+  return h
+    .response({
+      status: 'fail',
+      message: 'Buku tidak ditemukan',
+    })
+    .code(404);
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
